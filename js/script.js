43e30752cefa2fbfbdc168bbef6c5f1ce96b8d5e
Xaliq01 <jfarlixaliq@gmail.com>
2024-08-17 20:21:25 +0400

    Practise

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let a = document.getElementById('A1')
+
+function b(){
+    if(a.value == a.value){
+
+            console.log(a.value * 10) 
+           
+    }else{
+        alert('Напиши число меньше или равно 100')
+    }
+}
+
